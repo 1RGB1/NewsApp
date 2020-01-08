a4fe001c6116d510276823b562511440fc21fda9
Ahmad Ragab <a.ragab2012@gmail.com>
2020-01-08 11:03:43 +0200

    Starting News App

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ahmad Ragab on 1/8/20.
+//  Copyright © 2020 Ahmad Ragab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
