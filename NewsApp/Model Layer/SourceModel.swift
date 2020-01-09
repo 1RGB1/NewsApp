b5872e0d66c169ae22ffdce799a5c18119f85df5
Ahmad Ragab <a.ragab2012@gmail.com>
2020-01-09 11:38:08 +0200

    Handle mapping for sources model

--- NewsApp/Model Layer/SourceModel.swift
+++ NewsApp/Model Layer/SourceModel.swift
@@ -12,11 +12,21 @@ class SourceModel: Mappable {
     
     var id: String?
     var name: String?
+    var description: String?
+    var url: String?
+    var category: String?
+    var language: String?
+    var country: String?
     
     required init?(map: Map) {}
     
     func mapping(map: Map) {
         id <- map["id"]
         name <- map["name"]
+        description <- map["description"]
+        url <- map["url"]
+        category <- map["category"]
+        language <- map["language"]
+        country <- map["country"]
     }
 }
