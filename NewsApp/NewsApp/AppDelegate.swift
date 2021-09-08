2cd8f65318b74c97dfed3e207ceb8495c833cc0f
A.RGB <ahmed.ragab-bayoumi@vodafone.com>
2021-09-08 21:00:23 +0200

    Adding initial xcode project

---
+++ NewsApp/NewsApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  NewsApp
+//
+//  Created by Ahmad Ragab on 08/09/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
